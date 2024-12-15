5f784c7155b5c123ed12c377a10f11fc9573dd2c
60hzlapt0p <160487493+60hzlapt0p@users.noreply.github.com>
2024-12-15 20:03:32 +0100

    Add files via upload

---
+++ .emacs.d/lisp/init-auctex.el
@@ -0,0 +1,10 @@
+;;; init-auctex.el --- Auctex customisations -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package auctex
+  :ensure t
+  :defer t)
+
+(provide 'init-auctex)
+;;; init-auctex.el ends here
